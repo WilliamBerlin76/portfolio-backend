51306edb907c1a1303f4dff531c4e033569d0ecb
WilliamBerlin76 <william.berlin76@gmail.com>
2019-12-11 15:39:08 -0600

    replaced route paths with environment variables

--- api/api-router.js
+++ api/api-router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const authRouter = require('../auth/auth-router');
 const messageRouter = require('../routes/messagesRouter')
-router.use('/auth', authRouter);
-router.use('/messages', messageRouter);
+router.use(`${process.env.API_AUTH_ROUTE}`, authRouter);
+router.use(`${process.env.API_MESSAGES_ROUTE}`, messageRouter);
 
 module.exports = router;
